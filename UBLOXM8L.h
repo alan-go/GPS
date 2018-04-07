a659e410c7080418ce4c874485b59750c9c2d890
alan <haopher@gmail.com>
2018-04-07 10:21:34 +0800

    a better version to grab data

--- UBLOXM8L.h
+++ UBLOXM8L.h
@@ -82,13 +82,13 @@ private:
             printf ( "successfully opened port %s\n", serialPort.c_str() );
 
             char buffer[1024], GPS_Buffer[512];
-            int length = 0;
             int ind = 0;
-            int flag = 0,flagNew = 0;//0:null,1:NMEA,2:UBX;
-
+            int flag = 0;//0:null,1:NMEA,2:UBX;
+			char bufferUBX[2048], bufferNMEA[128];
+			int lengthUBX=0,lengthNMEA = 0;
+			u_int16_t* lengthUBXProtocol;
             while ( !stopMessageCapture() ) {
                 char tmp[128];
-                char UBX_Buffer[512], NMEA_Buffer[128];
                 auto transferred = sp.read_some ( boost::asio::buffer ( tmp ) );
                 if ( transferred <= 0 ) {
                     usleep ( 100 );
@@ -96,75 +96,54 @@ private:
                     continue;
                 }
 
-                int start = 0, end = 0;
-                if ( length>950 ) {
-                    length = 0;
-                    flag = 0;
-                    flagNew = 0;
-                }
-                memcpy ( buffer + length, tmp, transferred );
-                length += transferred;
+              //  printf ( "transferred = %d, flag = %d\n",transferred,flag );
 
-                printf ( "\ntransferred = %d,length = %d, flag = %d\n",transferred,length,flag );
+                for ( int i = 0; i<transferred; i++ ) {
 
-                //int startUBX=0,endUBX = 0,startNMEA = 0,endNMEA = 0;
-                bool moveHead = false;
-                for ( int i = length-transferred; i<length; i++ ) {
-                    bool getNew = false;
-
-                    if ( '$'==buffer[i]&&'G'==buffer[i+1] ) {
-                        getNew = true;
-                        flagNew = 1;
-                        printf ( "\nnew flag = 1, i = %d\n",i );
+                    if ( '$'==tmp[i]&&'G'==tmp[i+1] ) {
+                        flag = 1;
+						lengthNMEA = 0;
+                        printf ( "\n\nflag -> 1, i = %d\n",i );
                     }
-                    if ( ( u_char ) buffer[i] == 0xB5 && ( u_char ) buffer[i+1] == 0x62 ) {
-                        getNew = true;
-                        flagNew = 2;
-                        printf ( "\nnew flag = 2, i = %d\n",i );
+                    if ( ( u_char ) tmp[i] == 0xB5 && ( u_char ) tmp[i+1] == 0x62 ) {
+                        flag = 2;
+						lengthUBX = 0;
+						*lengthUBXProtocol = 10;
+                        printf ( "\n\nflag -> 2, i = %d\n",i );
 
                     }
-                    if ( 1==flag && ( '\n'==buffer[i]||'\r'==buffer[i] ) ) {
-                        printf ( "\nget a r/n, i = %d,start = %d, oldflag = %d\n",i,start,flag );
-
-                        end = i;
-						int l = end-start;
-                        if ( l<128 ) {
-                            printf ( "\ncopy NMEA : %d -- %d, l = %d \n",start,i-1,l );
-                            memcpy ( NMEA_Buffer,buffer+start,l );
-                            flagNew = 0;
-							for(int k = 0;k<l;k++){
-								printf("%c",NMEA_Buffer[k]);
-							}
-                        }
+                    if(1==flag){
+						bufferNMEA[lengthNMEA] = tmp[i];
+						lengthNMEA++;
                     }
-                    if ( getNew ) {
-                        printf ( "getNew: start = %d, i  = %d, oldflag = %d\n",start,i,flag );
-                        moveHead = true;
-                        end = i-1;
-						int l = end-start;
-                        if ( end>start&&flag!=0 ) {
-                            if ( 2==flag && l<512 ) {
-								printf ( "\ncopy ubx-%02x : %d -- %d, l=%d\n",(u_char) buffer[start+2],start,i-1,l );
-                                memcpy ( UBX_Buffer,buffer+start,l );
-								for(int k = 0;k<l;k++){
-									printf("%02x ",(u_char) UBX_Buffer[k]);
-								}
-                            }
-                        }
-                        start = i;
-                        getNew = false;
-                    }
+                    if(2==flag){
+						bufferUBX[lengthUBX] = tmp[i];
+						if(5==lengthUBX){
+							//length is writen in 4,5 in protocol
+							lengthUBXProtocol = (u_int16_t*)(bufferUBX+4);
+							//printf("lengthUBXProtocol = %d\n",*lengthUBXProtocol);
 
-                    flag = flagNew;
+						}
+						lengthUBX++;
+                    }
+                    if(('\n'==tmp[i]||'\r'==tmp[i])&&1==flag){
+						printf("get a NMEA,l = %d, i = %d\n",lengthNMEA,i);
+						for(int k = 0;k<lengthNMEA;k++){
+							printf("%c",bufferNMEA[k]);
+						}
+						flag=0;
+                    }
+                    //there are 8 extra bytes besides the playload;
+					if(lengthUBX==*lengthUBXProtocol+8 && 2==flag){
+						printf("get a UBX,l = %d, i = %d\n",lengthUBX, i);
+						if(lengthUBX<512)
+						for(int k = 0;k<lengthUBX;k++){
+							printf("%02x ",(u_char) bufferUBX[k]);
+						}
+						flag=0;
+					}
 
                 }
-                if ( moveHead ) {
-					printf("\nmove: %d -> 0, rest = %d",start,length-start);
-                    memcpy ( buffer,buffer+start,1024-start );
-                    length-=start;
-                    start = 0;
-                }
-
 
             }
             sp.close();
